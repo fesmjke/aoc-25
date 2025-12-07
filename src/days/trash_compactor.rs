9ca9fee7c59be18b3e29189c1e19319674b4d65d
fesmjke <fatalerror.sm@gmail.com>
2025-12-07 13:43:18 +0000

    feat: solved Day 6: Trash Compactor part b

--- src/days/trash_compactor.rs
+++ src/days/trash_compactor.rs
@@ -23,11 +23,12 @@ impl FromStr for Operation {
 }
 
 impl Day for TrashCompactor {
-    type Input = (Vec<Vec<u128>>, Vec<Operation>);
+    type Input = (Vec<Vec<u128>>, Vec<Operation>, Vec<String>);
 
     fn parse(lines: impl Iterator<Item = std::io::Result<String>>) -> Self::Input {
         let mut nums = vec![];
         let mut operations = vec![];
+        let mut raw = vec![];
 
         lines.filter_map(|line| line.ok()).for_each(|line| {
             if line.contains("*") || line.contains("+") {
@@ -40,13 +41,15 @@ impl Day for TrashCompactor {
             } else {
                 nums.push(parse_numbers_from_str::<u128>(&line).unwrap());
             }
+
+            raw.push(line);
         });
 
-        (nums, operations)
+        (nums, operations, raw)
     }
 
     fn part_a(input: &Self::Input) -> impl std::fmt::Display {
-        let (nums, ops) = input;
+        let (nums, ops, _) = input;
 
         let nums_length = nums[0].len();
         let mut result: Vec<u128> = vec![0; nums_length];
@@ -70,8 +73,68 @@ impl Day for TrashCompactor {
     }
 
     fn part_b(input: &Self::Input) -> impl std::fmt::Display {
-        -1
+        let (_, _, raw) = input;
+
+        let ops_line = &raw[raw.len() - 1];
+        let rows = raw.len() - 1; // total rows - operation line
+        let mut result = 0;
+        let mut current_op = Operation::Add;
+        let mut local_result = vec![];
+        let ops_line_len = ops_line.len();
+
+        for (i, _) in ops_line.chars().enumerate() {
+            let mut num = String::new();
+            let local_op = &ops_line[i..=i];
+
+            match Operation::from_str(local_op) {
+                Ok(op) => {
+                    match current_op {
+                        Operation::Add => {
+                            result += local_result.iter().sum::<u128>();
+                        }
+                        Operation::Mul => {
+                            result += local_result.iter().fold(1, |acc, e| acc * e);
+                        }
+                    }
+
+                    local_result.clear();
+                    current_op = op;
+                }
+                Err(_) => {}
+            };
+
+            for j in 0..rows {
+                num.push_str(&raw[j][i..=i]);
+            }
+
+            match current_op {
+                Operation::Add => {
+                    local_result.push(num.trim().parse::<u128>().unwrap_or(0));
+                }
+                Operation::Mul => {
+                    local_result.push(num.trim().parse::<u128>().unwrap_or(1));
+                }
+            }
+        }
+
+        match current_op {
+            Operation::Add => {
+                result += local_result.iter().sum::<u128>();
+            }
+            Operation::Mul => {
+                result += local_result.iter().fold(1, |acc, e| acc * e);
+            }
+        }
+
+        result
     }
 }
 
-aoc_test!(TrashCompactor, "day6", 4277556, 0, 0, 0);
+aoc_test!(
+    TrashCompactor,
+    "day6",
+    4277556,
+    3263827,
+    6343365546996 as u128,
+    11136895955912 as u128
+);
